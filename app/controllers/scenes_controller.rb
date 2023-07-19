fd66ade2a08bc2d7fea95bce2b1de85ffa64564b
Skye <anastasia_sorokina_90@mail.ru>
2023-07-19 11:52:13 +0300

    gameplay all fixed

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -4,8 +4,9 @@ class ScenesController < ApplicationController
 
   def show
     if scene.id != session[:scene_id] && !params[:force]
-      redirect_to scene_path(session[:scene_id])
+      redirect_to session[:scene_id] ? scene_path(session[:scene_id]) : root_path
     end
+    session[:character_stats] = {} if params[:force]
     session[:scene_id] = scene.id
   end
 
